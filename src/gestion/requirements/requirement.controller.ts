cd6663fd393a9574cf1c45ac241789ce81bf1a34
all-event-company <alleventcompany000@gmail.com>
2025-06-12 10:47:05 +0100

    simple update

--- src/gestion/requirements/requirement.controller.ts
+++ src/gestion/requirements/requirement.controller.ts
@@ -2,35 +2,29 @@ import { Controller, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Secure } from 'src/common/decorators/secure.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
-import { RequirementService } from './requirement.service';
 
 @Controller('requirements')
 @Secure('ACTIVE_USER')
 @UseGuards(RolesGuard)
 @Roles('DEVELOPER', 'LEAD_DEVELOPER')
 export class RequirementController {
-  constructor(private readonly requirementService: RequirementService) {}
-
+  // constructor(private readonly requirementService: RequirementService) {}
   // @Post()
   // create(@Body() dto: CreateRequirementDto) {
   //   return this.requirementService.create(dto);
   // }
-
   // @Get()
   // findAll() {
   //   return this.requirementService.findAll();
   // }
-
   // @Get(':id')
   // findOne(@Param('id') id: string) {
   //   return this.requirementService.findById(id);
   // }
-
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() dto: UpdateRequirementDto) {
   //   return this.requirementService.update(id, dto);
   // }
-
   // @Delete(':id')
   // delete(@Param('id') id: string) {
   //   return this.requirementService.delete(id);
